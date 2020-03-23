eccfa272a05b8c987ba91d6b9757ba4df7f88cbd
Jeremy Vijay Wongso <37033334+jeremyvw@users.noreply.github.com>
2020-03-24 00:22:35 +0700

    Try using relative path

--- Tugas4/add_file.py
+++ Tugas4/add_file.py
@@ -11,9 +11,9 @@ print(sys.stderr, 'Connecting to %s port %s' % server_addr)
 sock.connect(server_addr)
 
 try:
-    # filename = 'file2.txt'
-    this_folder = os.path.dirname(os.path.abspath(__file__))
-    filename = os.path.join(this_folder, 'file2.txt')
+    filename = 'file2.txt'
+    # this_folder = os.path.dirname(os.path.abspath(__file__))
+    # filename = os.path.join(this_folder, 'file2.txt')
     # filename='F:/Jeremy/Kuliah/Smt.6/Progjar/PROGJAR_05111740000062/Tugas4/file2.txt'
     tmp = open(filename, 'rb')
     file = tmp.read(2048)
